5ae0a70c27a78ca51b82c9dbce8f3718f179eb95
LHBR <lucas.rocha5@estudante.ifms.edu.br>
2023-09-19 11:42:34 -0400

    atualizado

--- src/componentes/Footer1/Footer1.tsx
+++ src/componentes/Footer1/Footer1.tsx
@@ -3,7 +3,7 @@ import './Footer1.css'
 function Footer1(){
     return(
         <footer className='footer'>
-        <div className='nome'>Lucas Rocha</div>
+        <div className='nome'>Portifólio</div>
         <div className='ctt'>Contato</div>
 
          
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,10 +1,82 @@
+import { useState } from "react";
+import Projeto from "../projeto/Projeto";
 import './Main.css'
+type ProjetoType = {
+    id: number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
+export default function Main(){
+    //Hook
+    const [texto,setTexto] = useState("")
 
-function Main(){
+    const projeto:ProjetoType[] = [
+        {
+            id:1,
+            titulo:"Projeto Ensino",
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:"/projetoEnsino"
+        },
+        {
+            id:2,
+            titulo:"Semana do Meio Ambiente",
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:"/transformes.jpeg"
+        },
+        {
+            id:3,
+            titulo:" 2º Dia da Consciência Indígena",
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:"/transformes.jpeg"
+        },
+        {
+          id:4,
+          titulo:" 2º Dia da Consciência Indígena",
+          sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+          imagem:"/transformes.jpeg"
+      },
+      {
+        id:5,
+        titulo:" 2º Dia da Consciência Indígena",
+        sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+        imagem:"/transformes.jpeg"
+    },
+    {
+      id:6,
+      titulo:" 2º Dia da Consciência Indígena",
+      sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+      imagem:"/transformes.jpeg"
+  },
+
+
+
+    ]
+    //A função recebe um atributo chamado e de "event"
+    function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
+        console.log(e.target.value)
+        setTexto(e.target.value)
+    }
     return(
-        <main><div className='textm'>Olá, eu sou o Lucas Rocha, estudo no IFMS campus Naviraí, cursando o curso técnico em informática para internet.
-         Atualmente estou lançando um aplicativo de mobilidade e serviços chmado uzap.  </div></main>
+        <>
+            <div className="pesquisa">
+                
+                <p>Buscar Filme</p>
+                <input className='barrapesquisa' type="text" onChange={mudaTexto} />
+                <div>
+                    <p className='texto_digitado'>pesquisa: {texto}</p>
+                </div>
+            </div>
+            <main className="content-main">
+                {projeto.filter((projeto)=>projeto.titulo.toLowerCase().includes(texto.toLowerCase())).map((filme:ProjetoType)=>
+                    <Projeto key={filme.id} 
+                           titulo={filme.titulo} 
+                           sinopse={filme.sinopse} 
+                           imagem={filme.imagem}
+                    />
+                    )
+                }
+            </main>
+        </>
     )
 }
-
-export default Main
